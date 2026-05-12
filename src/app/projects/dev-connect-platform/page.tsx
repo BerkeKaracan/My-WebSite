89f89010dd88bf473e6e0cf8aa29ca55b54ecca5
BerkeKaracan <berkekaracan1113@gmail.com>
2026-05-12 14:07:31 +0300

    fix: add temporary \dev-connect-platform\page.tsx

---
+++ src/app/projects/dev-connect-platform/page.tsx
@@ -0,0 +1,5 @@
+<h4 className="text-base font-semibold text-zinc-100 group-hover/card:text-white transition-colors">
+  <span className="before:absolute before:inset-0 before:z-10 cursor-default">
+    DevConnect Platform (Coming Soon)
+  </span>
+</h4>;
